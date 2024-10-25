5d7181f8b03ee4dd9f2c76688e402e36f1bdcb52
Tegue-M <tegue@synthesis.co.za>
2024-10-25 09:59:09 +0200

    Adding buildspec

---
+++ simple-node-app/index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/node', (req, res) => {
+    res.send('Hello from simple-node-app!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
