e9301f0adf862981003b48070a9d472f5a711005
Tegue-M <tegue@synthesis.co.za>
2024-10-25 13:37:42 +0200

    Dev hello: updated

--- simple-node-app/index.js
+++ simple-node-app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/node', (req, res) => {
-    res.send('Dev env says hello, hello from simple-node-app!');
+    res.send('Dev updated - hello from simple-node-app!');
 });
 
 app.listen(PORT, () => {
