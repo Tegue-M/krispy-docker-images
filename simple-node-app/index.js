4f67b8d373dca6b62bf9a2cbbb2b3f75f4a63524
Tegue-M <tegue@synthesis.co.za>
2024-10-29 14:28:53 +0200

    Testing pipeline

--- simple-node-app/index.js
+++ simple-node-app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/node', (req, res) => {
-    res.send('Hello from main repo simple-node-app!');
+    res.send('Hello, this is a demo!');
 });
 
 app.listen(PORT, () => {
